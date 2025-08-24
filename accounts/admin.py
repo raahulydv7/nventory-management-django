3ac2dffc9d057d8fb8014d5e4b3f7accdeb5ee3b
raahulydv7 <rahul7879yadav@gmail.com>
2025-08-25 03:06:12 +0530

    admin :register User  model and add into base.py setting

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,1 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(DjangoUserAdmin):
-
-    list_display = ("username", "email", "role", "is_staff", "is_superuser", "is_active")
-    list_filter = ("role", "is_staff", "is_superuser", "is_active")
-
-    fieldsets = DjangoUserAdmin.fieldsets + (
-        ("Application Role", {"fields": ("role",)}),
-    )
-    add_fieldsets = DjangoUserAdmin.add_fieldsets + (
-        (None, {"fields": ("role",)}),
-    )
